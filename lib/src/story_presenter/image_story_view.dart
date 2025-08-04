e3fb914961bd862612d2d93ac1ddc61725a6cbc6
himmatsingh019 <himmatsingh19k@gmail.com>
2025-08-04 20:10:51 +0530

    fix

--- lib/src/story_presenter/image_story_view.dart
+++ lib/src/story_presenter/image_story_view.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 import 'dart:io';
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_story_presenter/flutter_story_presenter.dart';
